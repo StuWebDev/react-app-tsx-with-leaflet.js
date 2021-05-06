eaa97665eb2ebdd1c891ad68a61da645f9ed821f
Stu <h.s.j.wilkes@gmail.com>
2021-05-06 14:44:10 +0100

    add leaflet.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="mapid"></div>
     </div>
   );
 }
