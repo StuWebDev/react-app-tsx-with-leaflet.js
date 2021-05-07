753823301facc17622162ff4dbc253e00f4b6f09
Stu <h.s.j.wilkes@gmail.com>
2021-05-07 09:43:56 +0100

    rm leaflet from index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
+
 export default class App extends React.Component {
+
+  componentDidMount() {
+    // code to run just after the component "mounts" / DOM elements are created
+    // we could make an AJAX request for the GeoJSON data here if it wasn't stored locally
+    
+    // create the Leaflet map object
+
+  }
+
  render(){
-  return <MapContainer className="map" center={[53.3915, -2.125143]} zoom={13} scrollWheelZoom={false}>
+  return <MapContainer className="map" center={[53.3915, -2.125143]} zoom={12} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[51.505, -0.09]}>
+          <Marker position={[53.3915, -2.125143]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
