3dbe2752acbae1e1a4a599fda5db8714c8d28482
Stu <h.s.j.wilkes@gmail.com>
2021-05-14 15:11:23 +0100

    webpack bundle main & fallback.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 const zlib = require("zlib")
 const path = require('path')
+const { renameSync } = require('fs')
+
+const pluginName = 'RenameFilePlugin';
+class RenameFilePlugin {
+  constructor(options = {}) {
+    this.options = { ...options }
+   }
+
+  apply(compiler) {
+    compiler.hooks.done.tap(pluginName, () => {
+      renameSync(this.options.startFile, this.options.renameTo)
+    })
+  }
+}
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.tsx'],
@@ -55,15 +69,27 @@ module.exports = {
   },
   plugins: [
     new CompressionPlugin({
-      filename: '[path][name].js',
+      filename: '[path][name].br',
       test: /\.js$/,
       algorithm: "brotliCompress",
       compressionOptions: {
         params: {
           [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
         },
-      },
-      deleteOriginalAssets: true
+      }
+    }),
+    new CompressionPlugin({
+      filename: '[path][name].gz',
+      test: /\.js$/,
+      algorithm: "gzip"
+    }),
+    new RenameFilePlugin({
+      startFile:`dist/main-bundle.br`,
+      renameTo:`dist/main-bundle.js`
+    }),
+    new RenameFilePlugin({
+      startFile:`dist/main-bundle.gz`,
+      renameTo:`dist/fallback.js`
     }),
     new HtmlWebpackPlugin({
       filename: "index.html",
